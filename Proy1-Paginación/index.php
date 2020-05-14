07769e9e5257f36702180506faf13aa5170e61ff
Jose Valencia <joseantonio9850@hotmail.com>
2020-05-13 21:38:35 -0500

    Práctica paginación

---
+++ Proy1-Paginación/index.php
@@ -0,0 +1,42 @@
+<?php
+
+try{
+
+    $conexion = new PDO('mysql:host=localhost;dbname=paginacion', 'joseantonio098', '');
+    
+    // ---> Estableciendo la página ingresada
+    $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+    $postPorPagina = 5;
+
+    // ---> Determina el Nro incial del artículo por página
+    $inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0;
+
+    // ---> Traemos todo los artículos, donde $inicio es el Nro incial de artículos
+    $articulos = $conexion->prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $postPorPagina");
+
+    // ---> Ejecutamos consulta
+    $articulos->execute();
+    $articulos = $articulos->fetchAll(); 
+
+    // ---> Si la página no contiene articulos, redireccionamos a index.php
+    if(!$articulos){
+        header('Location: index.php');
+    }
+
+    // ---> Muestra el total de artículo de la base de datos
+    $totalArticulos = $conexion->query('SELECT  FOUND_ROWS() as total');
+    $totalArticulos = $totalArticulos->fetch()['total'];
+
+    // ---> Muestra el total de páginas
+    $numeroPaginas = ceil($totalArticulos / $postPorPagina);
+
+
+}catch(PDOException $e){
+    echo 'Error: '. $e->getMessage();
+    die();
+}
+
+
+require 'index.view.php';
+
+?>
---
+++ Proy1-Paginación/index.view.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Paginación </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contenedor">
+        <h1> Artículos </h1>
+        <section class="articulos">
+            <ul>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Et</li>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Et</li>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Et</li>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Et</li>
+                <li>1.- Lorem ipsum dolor sit amet consectetur adipisicing elit. Et</li>
+            </ul>
+        </section>
+
+        <section class="paginacion">
+            <ul>
+                <li class="disabled"> &laquo; </li>
+                <li class="active"><a href="#"> 1 </a></li>
+                <li><a href="#"> 2 </a></li>
+                <li><a href="#"> 3 </a></li>
+                <li><a href="#"> 4 </a></li>
+                <li><a href="#"> &raquo; </a></li>
+            </ul>
+        </section>
+    </div>
+</body>
+</html>
